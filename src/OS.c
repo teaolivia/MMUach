6b25179012c081af5da693891c25f2163c1d1828
rmxhaha <code000123@gmail.com>
2016-03-09 13:48:55 +0700

    beatuify version of OS using struct

--- src/OS.c
+++ src/OS.c
@@ -12,14 +12,34 @@
 #include <sys/stat.h>
 #include "PageTable.h"
 
-void chooseVictim()){
-	//
-	//
-	// ALGORITHM START HERE
-}
 
-int shmid, OSID, frame, page, MMid, disk, reqpage, finished;
-page_table_pointer pagetable;
+typedef struct OS_t {
+	int shmid, OSID, frame, page, MMid, disk, reqpage, finished;
+	page_table_pointer pagetable;	
+} OS_t;
+
+OS_t* os = NULL;
+
+void initOs(int page, int frame){
+	os = malloc(sizeof(OS_t));
+
+	os->page = page;
+	os->frame = frame;
+	os->finished = 0;
+	os->disk = 0;
+	os->OSID = getpid();
+	os->shmid = shmget(os->OSID,os->page*sizeof(page_table_entry),IPC_CREAT|0666);
+	os->pagetable = shmat(os->shmid,0,0);
+
+	// init page table
+	int i;
+	for(i=0; i<page; i++){
+		os->pagetable[i].Valid = 0;
+		os->pagetable[i].Frame = -1;
+		os->pagetable[i].Dirty = 0;
+		os->pagetable[i].Requested = 0;
+	}
+}
 
 void PrintPageTable(page_table_entry PageTable[],int NumberOfPages) {
 
@@ -33,23 +53,15 @@ PageTable[Index].Requested);
 
 }
 
-void init(int p){
-	int i;
-	for(i=0; i<p; i++){
-		pagetable[i].Valid = 0;
-		pagetable[i].Frame = -1;
-		pagetable[i].Dirty = 0;
-		pagetable[i].Requested = 0;
-	}
-}
 
-void continueExec(){
-	MMid = 0;
+void continueExec(int p){
+	int MMid = 0;
 	//scan pada page table untuk menemukan field Requested
 	int i=0;
-	while(i<page){
-		if(pagetable[i].Requested!=0){
-			MMid = pagetable[i].Requested;
+	int reqpage;
+	while(i<os->page){
+		if(os->pagetable[i].Requested!=0){
+			MMid = os->pagetable[i].Requested;
 			reqpage = i;
 			break;
 		} else {
@@ -59,82 +71,73 @@ void continueExec(){
 	
 	if(MMid==0){
 		printf("The MMU has finished\n");
-		PrintPageTable(pagetable,page);
+		PrintPageTable(os->pagetable,os->page);
 		//Clear Shm
 		//Count disk access
-		printf("%d disk accesses required\n",disk);
-		finished = 1;
+		printf("%d disk accesses required\n",os->disk);
+		os->finished = 1;
 	} else {
 		printf("Process %d has requested page %d\n",MMid,reqpage);
 		//count frame
 		int i;
 		int count=0;
-		for(i=0; i<page; i++){
-			if(pagetable[i].Frame!=-1){
+		for(i=0; i<os->page; i++){
+			if(os->pagetable[i].Frame!=-1){
 				count++;
 			}
 		}
 		
 		//Check Frame
-		if(count>=frame){
+		if(count>=os->frame){
 			//frame is full, swap
 			
 			
 			//Search Frame
 			int i=0;
-			while(pagetable[i].Frame==-1){
+			while(os->pagetable[i].Frame==-1){
 				i++;
 			}
 			printf("Chose a victim page %d\n",i);
 			
 			
 			//check if it is dirty
-			if(pagetable[i].Dirty!=0){
+			if(os->pagetable[i].Dirty!=0){
 				printf("Victim is dirty, write out\n");
 				sleep(1);
-				pagetable[i].Dirty = 0;
-				printf("Put in victim's frame %d\n",pagetable[i].Frame);
-				disk++;
+				os->pagetable[i].Dirty = 0;
+				printf("Put in victim's frame %d\n",os->pagetable[i].Frame);
+				os->disk++;
 			}
-			pagetable[reqpage].Frame = pagetable[i].Frame;
-			pagetable[reqpage].Valid = 1;
-			pagetable[reqpage].Requested = 0;
+			os->pagetable[reqpage].Frame = os->pagetable[i].Frame;
+			os->pagetable[reqpage].Valid = 1;
+			os->pagetable[reqpage].Requested = 0;
 			
-			pagetable[i].Valid = 0;
-			pagetable[i].Frame = -1;
-			pagetable[i].Requested = 0;
+			os->pagetable[i].Valid = 0;
+			os->pagetable[i].Frame = -1;
+			os->pagetable[i].Requested = 0;
 			
 			
-			disk++;
+			os->disk++;
 			kill(MMid,SIGCONT);
 		} else {
 			//allocate to some frame
 			
-			pagetable[reqpage].Frame = count;
-			pagetable[reqpage].Valid = 1;
-			pagetable[reqpage].Requested = 0;
+			os->pagetable[reqpage].Frame = count;
+			os->pagetable[reqpage].Valid = 1;
+			os->pagetable[reqpage].Requested = 0;
 			printf("Put in free frame %d\n",count);
 			printf("Unblock MMU\n");
-			disk++;
+			os->disk++;
 			kill(MMid,SIGCONT);
 		}
 	}
 }
 
 int main(int argc,char *argv[]){
-	finished = 0;
-	disk = 0;
+	initOs(atoi(argv[1]), atoi(argv[2]) );
 	signal(SIGUSR1,continueExec);
-	page = atoi(argv[1]);
-	frame = atoi(argv[2]);
-	OSID = getpid();
-	printf("The shared memory key (PID) is %d\n",OSID);
-	shmid = shmget(OSID,page*sizeof(page_table_entry),IPC_CREAT|0666);
-	pagetable = shmat(shmid,0,0);
-	init(page);
+	printf("The shared memory key (PID) is %d\n", os->OSID);
 	
-	while(finished==0){
-		
-	}
+	while(!os->finished);
 	return 0;
 }
